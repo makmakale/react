3046f4d55c1adff13c4618f6986217b627469d47
Maksim Makarenko <150532778+makmakale@users.noreply.github.com>
2025-01-23 14:22:54 +0300

    Add files via upload

---
+++ tetris/src/styles/TetrisBoard.js
@@ -0,0 +1,23 @@
+import { mobileWidth } from '@/styles/_media';
+import styled from 'styled-components';
+
+export const StyledTetrisBoard = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+  background: linear-gradient(to right, #525356, #252526);
+  border-radius: 0;
+  overflow: hidden;
+  min-width: 370px;
+  min-height: 640px;
+  height: 100vh;
+  box-shadow: 0 0 30px rgba(0, 0, 0, .5);
+
+  @media (min-width: ${mobileWidth}px) {
+    border-radius: 16px;
+    margin: 0 auto;
+    height: unset;
+  }
+`;
